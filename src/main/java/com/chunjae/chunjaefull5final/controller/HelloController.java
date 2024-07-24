4bd1fce0feb0b5e65d5ca547a505d22f4f37033d
ysh <ysh71034@gmail.com>
2024-07-24 10:26:37 +0900

    react port Test

--- src/main/java/com/chunjae/chunjaefull5final/controller/HelloController.java
+++ src/main/java/com/chunjae/chunjaefull5final/controller/HelloController.java
@@ -19,10 +19,11 @@ public class HelloController {
         return "Hello, world!";
     }
 
-    @PostMapping("/api/item-img/item-list")
+    /** STEP2 문제목록 api 호출 **/
+    @PostMapping("/api/item-img/exam-list/item-list")
     public ResponseEntity<String> apiTest(@RequestBody Map<String, Object> requestData) {
         // 예시로 요청 데이터를 받아서 처리하는 부분
-        System.out.println("Received request data: " + requestData);
+//        System.out.println("Received request data: " + requestData);
 
         // 요청 데이터를 추출하여 원격 서버로 전송
         HttpHeaders headers = new HttpHeaders();
@@ -32,7 +33,7 @@ public class HelloController {
         // REST 템플릿을 사용하여 원격 서버로 POST 요청 전송
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> responseEntity = restTemplate.exchange(
-                "https://tsherpa.item-factory.com/item-img/item-list",
+                "https://tsherpa.item-factory.com/item-img/exam-list/item-list",
                 HttpMethod.POST,
                 requestEntity,
                 String.class
@@ -41,6 +42,30 @@ public class HelloController {
         // 처리 결과를 반환
         return responseEntity;
     }
+    /** STEP2 유사문제 api 호출 **/
+    @PostMapping("/api/item-img/similar-list")
+    public ResponseEntity<String> apiSimilar(@RequestBody Map<String, Object> requestData) {
+        // 예시로 요청 데이터를 받아서 처리하는 부분
+//        System.out.println("Received request data: " + requestData);
+
+        // 요청 데이터를 추출하여 원격 서버로 전송
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestData, headers);
+
+        // REST 템플릿을 사용하여 원격 서버로 POST 요청 전송
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                "https://tsherpa.item-factory.com/item-img/similar-list",
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        );
+
+        // 처리 결과를 반환
+        return responseEntity;
+    }
+
 
     @PostMapping("/back/savedpaper")
     public ResponseEntity<String> reactTest(@RequestBody Map<String, Object> requestData) {
