c01819b9a5cbc0e20a8209ea9b8ff91031a37c73
choi <cyh010217@gmail.com>
2024-07-30 12:32:55 +0900

    jwt 안되던거 수정

--- src/main/java/com/chunjae/chunjaefull5final/jwt/JWTFilter.java
+++ src/main/java/com/chunjae/chunjaefull5final/jwt/JWTFilter.java
@@ -3,6 +3,7 @@ package com.chunjae.chunjaefull5final.jwt;
 import com.chunjae.chunjaefull5final.domain.User;
 import com.chunjae.chunjaefull5final.domain.UserRole;
 import com.chunjae.chunjaefull5final.service.user.CustomUserDetails;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
@@ -40,29 +41,39 @@ public class JWTFilter extends OncePerRequestFilter {
                 }
             }
         }
-        if (token != null && !"".equals(token) && !jwtUtil.isExpired(token)) {
 
-            jwtUtil.refreshExpiredTime(response,token);
+        try {
 
-            //토큰에서 username과 role 획득
-            String username = jwtUtil.getUsername(token);
-            String role = jwtUtil.getRole(token);
 
-            //userEntity를 생성하여 값 set
-            User userEntity = new User();
-            userEntity.setEmail(username);
-            userEntity.setPwd("temppassword");
-            userEntity.setRole(UserRole.valueOf(role.split("_")[1]));
+            if (token != null && !"".equals(token) && !jwtUtil.isExpired(token)) {
 
-            //UserDetails에 회원 정보 객체 담기
-            CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
+                jwtUtil.refreshExpiredTime(response, token);
 
-            //스프링 시큐리티 인증 토큰 생성
-            Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
-            //세션에 사용자 등록
-            SecurityContextHolder.getContext().setAuthentication(authToken);
-        } else if(token != null && !"".equals(token) && jwtUtil.isExpired(token)){
-            jwtCookie.setMaxAge(0);
+                //토큰에서 username과 role 획득
+                String username = jwtUtil.getUsername(token);
+                String role = jwtUtil.getRole(token);
+
+                //userEntity를 생성하여 값 set
+                User userEntity = new User();
+                userEntity.setEmail(username);
+                userEntity.setPwd("temppassword");
+                userEntity.setRole(UserRole.valueOf(role.split("_")[1]));
+
+                //UserDetails에 회원 정보 객체 담기
+                CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
+
+                //스프링 시큐리티 인증 토큰 생성
+                Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+                //세션에 사용자 등록
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            } else if (token != null && !"".equals(token) && jwtUtil.isExpired(token)) {
+                jwtCookie.setMaxAge(0);
+            }
+        }catch (ExpiredJwtException e){
+            System.out.println(e);
+            if(jwtCookie != null) {
+                jwtCookie.setMaxAge(0);
+            }
         }
 
         filterChain.doFilter(request, response);
