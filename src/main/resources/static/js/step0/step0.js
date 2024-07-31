a10795b3eefff04e7797838ec6dcc1168e4adbb4
choi <cyh010217@gmail.com>
2024-07-31 17:55:49 +0900

    step0 수정중

--- src/main/resources/static/js/step0/step0.js
+++ src/main/resources/static/js/step0/step0.js
@@ -10,93 +10,94 @@ document.addEventListener('DOMContentLoaded', function () {
     let itemCount = 0;
     let itemInfoShow = false;
 
-    // 체크박스 상태 초기화
-    let resetCheckboxes = () => {
-        checkboxes.forEach(checkbox => {
-            checkbox.checked = false; // 모든 체크박스 해제
-        });
-        selectedItemCount = 0; // 선택된 문항 수 초기화
-        selectedExamIds = []; // 선택된 시험지 ID 배열 초기화
-        updateSelectedCount(); // 선택된 문항 수 업데이트
-    };
-
-    // '신규 시험지 만들기' 버튼 클릭시
-    newButton.addEventListener('click', function () {
-        // console.log('subjectId..',subjectId);
-        window.location.href = '/step1/select-chapter/' + subjectId;
-    })
-
-    let selectedItemCount = 0;  // 선택된 문항 수를 추적 변수
-    let selectedExamIds = [];   // 선택된 시험지 ID를 저장하는 배열
-
-
-    // 선택된 문항 수를 업데이트하는 함수
-    let updateSelectedCount = () => {
-        selectedCountElement.textContent = selectedItemCount;
-    };
-
-    checkboxes.forEach(checkbox => { // 각 체크박스에 이벤트 리스너를 추가
-        checkbox.addEventListener('change', function () {
-
-            itemCount = parseInt(this.getAttribute('item-cnt'));  // 체크한 문항 수 가져오기
-            let examId = this.getAttribute('exam-id');  // 체크한 시험지 ID 가져오기
-
-            if (this.checked) {
-
-                if (selectedItemCount + itemCount > maxItemCount) {   // 최대 문항 수를 초과할 경우 경고를 표시하고 체크 해제
-                    alert('최대 90문항까지 선택 가능합니다.');
-                    this.checked = false;
-                } else {   // 선택된 문항 수와 선택된 시험지 ID 배열에 추가
-                    selectedItemCount += itemCount;
-                    selectedExamIds.push(examId);
-                }
-            } else {
-                // 선택 해제 시 문항 수와 시험지 ID 배열에서 제거
-                selectedItemCount -= itemCount;
-                selectedExamIds = selectedExamIds.filter(id => id !== examId);
-            }
-
-            updateSelectedCount(); // 선택된 문항 수 업데이트
-        });
-    });
-
-
-    // '선택한 시험지 편집하기' 버튼 클릭 시
-    editButton.addEventListener('click', function () {
-        if (selectedItemCount === 0) {
-            alert('시험지를 선택해주세요. ' +
-                '최대 90문항까지 선택 가능합니다.');
-        } else {
-            console.log('선택된 시험지 ID:', selectedExamIds);
-
-            // 선택된 examId 값 넘기기
-            fetch('/step0/examId', {
-                method: 'POST'
-                , headers: {
-                    'Accept': 'application/json'
-                    , 'Content-Type': 'application/json'
-                }
-                , body: JSON.stringify({examIdList: selectedExamIds}) // JSON 문자열로 변환하여 전달
-            }).then(response => response.text())
-                .then(data => {
-                    console.log('응답!!!!', data);
-                    // 서버로 부터 데이터 성공적으로 받으면 step2로 이동
-                    /* !!!!!! 경로 변경시 수정 !!!!!*/
-                    window.location.href = 'http://localhost:8080/step2/edit/' + subjectId;
-                    // examId 없을 때 redirect 처리
-                }).catch(error => {
-                console.log('에러 발생', error);
-                alert('에러 발생..')
+    {
+        // 체크박스 상태 초기화
+        let resetCheckboxes = () => {
+            checkboxes.forEach(checkbox => {
+                checkbox.checked = false; // 모든 체크박스 해제
             });
-        }
-    });
+            selectedItemCount = 0; // 선택된 문항 수 초기화
+            selectedExamIds = []; // 선택된 시험지 ID 배열 초기화
+            updateSelectedCount(); // 선택된 문항 수 업데이트
+        };
 
-    // 초기 선택된 문항 수 업데이트
-    updateSelectedCount();
+        // '신규 시험지 만들기' 버튼 클릭시
+        newButton.addEventListener('click', function () {
+            // console.log('subjectId..',subjectId);
+            window.location.href = '/step1/select-chapter/' + subjectId;
+        })
 
-    // 페이지가 로드될 때 체크박스 초기화
-    window.addEventListener('load', resetCheckboxes);
+        let selectedItemCount = 0;  // 선택된 문항 수를 추적 변수
+        let selectedExamIds = [];   // 선택된 시험지 ID를 저장하는 배열
 
+
+        // 선택된 문항 수를 업데이트하는 함수
+        let updateSelectedCount = () => {
+            selectedCountElement.textContent = selectedItemCount;
+        };
+
+        checkboxes.forEach(checkbox => { // 각 체크박스에 이벤트 리스너를 추가
+            checkbox.addEventListener('change', function () {
+
+                itemCount = parseInt(this.getAttribute('item-cnt'));  // 체크한 문항 수 가져오기
+                let examId = this.getAttribute('exam-id');  // 체크한 시험지 ID 가져오기
+
+                if (this.checked) {
+
+                    if (selectedItemCount + itemCount > maxItemCount) {   // 최대 문항 수를 초과할 경우 경고를 표시하고 체크 해제
+                        alert('최대 90문항까지 선택 가능합니다.');
+                        this.checked = false;
+                    } else {   // 선택된 문항 수와 선택된 시험지 ID 배열에 추가
+                        selectedItemCount += itemCount;
+                        selectedExamIds.push(examId);
+                    }
+                } else {
+                    // 선택 해제 시 문항 수와 시험지 ID 배열에서 제거
+                    selectedItemCount -= itemCount;
+                    selectedExamIds = selectedExamIds.filter(id => id !== examId);
+                }
+
+                updateSelectedCount(); // 선택된 문항 수 업데이트
+            });
+        });
+
+
+        // '선택한 시험지 편집하기' 버튼 클릭 시
+        editButton.addEventListener('click', function () {
+            if (selectedItemCount === 0) {
+                alert('시험지를 선택해주세요. ' +
+                    '최대 90문항까지 선택 가능합니다.');
+            } else {
+                console.log('선택된 시험지 ID:', selectedExamIds);
+
+                // 선택된 examId 값 넘기기
+                fetch('/step0/examId', {
+                    method: 'POST'
+                    , headers: {
+                        'Accept': 'application/json'
+                        , 'Content-Type': 'application/json'
+                    }
+                    , body: JSON.stringify({examIdList: selectedExamIds}) // JSON 문자열로 변환하여 전달
+                }).then(response => response.text())
+                    .then(data => {
+                        console.log('응답!!!!', data);
+                        // 서버로 부터 데이터 성공적으로 받으면 step2로 이동
+                        /* !!!!!! 경로 변경시 수정 !!!!!*/
+                        window.location.href = 'http://localhost:8080/step2/edit/' + subjectId;
+                        // examId 없을 때 redirect 처리
+                    }).catch(error => {
+                    console.log('에러 발생', error);
+                    alert('에러 발생..')
+                });
+            }
+        });
+
+        // 초기 선택된 문항 수 업데이트
+        updateSelectedCount();
+
+        // 페이지가 로드될 때 체크박스 초기화
+        window.addEventListener('load', resetCheckboxes);
+    }
 
     // 미리보기
     document.querySelectorAll('.tbody').forEach(tbody => {  // tbody에 이벤트 리스너 추가
@@ -256,7 +257,6 @@ document.addEventListener('DOMContentLoaded', function () {
                         });
 
                     })
-
                     .catch(error => {
                         alert("미리보기를 불러오지 못했습니다. 관리자에게 문의해 주시기 바랍니다.");
                         console.error("Fetch error: ", error);
@@ -264,6 +264,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     .finally(() => {
                         // document.querySelector(".loading-cnt").style.display = 'none'; // 로딩 표시 제거
                     });
+
 
                 // 문항 정보표 데이터 불러오기
                 fetch('/preview/info', {
